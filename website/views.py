daadd83841d73e4df83ddcc8b6e273f933599c10
David Forrester <david.o.forrester@gmail.com>
2024-03-19 17:01:47 +1100

    Creates image and displays on website

--- website/views.py
+++ website/views.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, request
+from website.visuals import CreateVisual
 from .models import StrategiseYourLife
 from . import db
 
@@ -12,12 +13,21 @@ def home():
     importance = request.form.get('importance')
     timeInvested = request.form.get('time-invested')
 
-    #if strageticLifeUnit == ""
-    newStrategiseYourLife = StrategiseYourLife(strategicLifeUnits=strageticLifeUnit, satisfaction=satisfaction, importance=importance, timeInvested=timeInvested)
-    db.session.add(newStrategiseYourLife)
-    db.session.commit()
+    print(request.form)
 
-    #run the python visual code here
-    print('data saved')
+    if satisfaction == "":
+      print('missing satisfaction data')
+    elif importance == "":
+      print('missing importance data')
+    elif timeInvested == "":
+      print('missing time invested data')
+    else:
+      newStrategiseYourLife = StrategiseYourLife(strategicLifeUnits=strageticLifeUnit, satisfaction=satisfaction, importance=importance, timeInvested=timeInvested)
+      db.session.add(newStrategiseYourLife)
+      db.session.commit()
+      
+      #run the python visual code here
+      print('data saved')
+      CreateVisual()
 
   return render_template("home.html")
---
+++ website/visuals.py
@@ -0,0 +1,44 @@
+import numpy as np
+import pandas as pd
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import sqlite3
+
+
+def CreateVisual():
+  conn = sqlite3.connect('instance/database.db')
+
+  df = pd.read_sql('SELECT * FROM strategise_your_life', conn)
+  
+  #defining the colours for the categories
+
+  fig = plt.figure(figsize=(15,15))
+  ax = fig.add_subplot(111)
+
+  #Setting up the text on the plot
+  plt.title('Strategise Your Life')
+  plt.xlabel('Satisfaction')
+  plt.ylabel('Importance')
+
+  #x and y limits
+  plt.xlim((0,10))
+  plt.ylim((0,10))
+
+  #adding the 4 quadrant lines
+  plt.plot([5,5],[0,11], linewidth=1, color='grey' )
+  plt.plot([0,11],[5,5], linewidth=1, color='grey' )
+
+  #Plots the data points
+  plt.scatter(df['satisfaction'], df['importance'], df['timeInvested']*10000),# c=colour_list)
+
+  #adds the labels to the points
+  for i, txt in enumerate(df['strategicLifeUnits']):
+    ax.annotate(txt, (df['satisfaction'][i], df['importance'][i]), xytext=(df['satisfaction'][i]+0.3, df['importance'][i]+0.3), arrowprops=dict(arrowstyle='-'))
+
+  # Turn off tick labels
+  ax.set_yticklabels([])
+  ax.set_xticklabels([])
+
+  plt.savefig('website/static/visual.png') 
+
